e5b245b2698c265da060d5def03c495c4f18a160
Christopher Solis <cjsolis_07@hotmail.com>
2017-08-08 18:28:11 -0600

    Commit1
    Físicas de las bolas de billar. Mesa de billar. Palo.

---
+++ Assets/Scripts/MoverBola2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverBola2 : MonoBehaviour {
+
+    private Rigidbody rb;
+    public int speed = 10;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        //bool moveHorizontal = Input.GetMouseButtonDown(0);
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        /*if (moveHorizontal)
+        {
+            Vector3 movimiento = new Vector3(speed, 0, 0);
+            rb.AddForce(movimiento);
+
+        }*/
+
+        Vector3 movimiento = new Vector3(moveHorizontal * speed, 0, moveVertical * speed);
+        rb.AddForce(movimiento);
+    }
+}
